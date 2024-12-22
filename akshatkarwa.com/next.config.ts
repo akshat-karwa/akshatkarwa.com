f088804b19cee2b533c253368ec55b4993649dfa
akarwa7 <akshatkarwa@gatech.edu>
2024-12-22 17:50:33 +0530

    changes to improve google search results

--- akshatkarwa.com/next.config.ts
+++ akshatkarwa.com/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+  },
+  compress: true,
+  poweredByHeader: false
 };
-
+module.exports = nextConfig
 export default nextConfig;
